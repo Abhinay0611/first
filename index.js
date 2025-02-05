70d77189334424b8c4d969476211b7bd4c8c4c49
Abhinay0611 <abhinaykumar0611@gmail.com>
2025-02-05 12:05:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT ||  5000
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+  res.send("hello")
+})
+app.get("/home",(req,res)=>{
+  res.send("Home")
+})
+app.get("/About",(req,res)=>{
+  res.send("About")
+})
+app.post("/api",async (req,res)=>{
+  const data = await req.body
+  res.status(500).json({hello:data,msg:"qwertyuioasdfghjkl"})
+})
+
+app.listen(port,()=>{
+console.log("server running");
+
+})
